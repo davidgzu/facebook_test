9b7735226b1b13f2cd639bb1de73812c109b64b3
David González <davidgonzalez@MacBook-Pro-de-David.local>
2016-11-24 20:42:23 -0600

    test facebook api

---
+++ main.py
@@ -0,0 +1,12 @@
+from usuario import Usuario
+
+user = Usuario()
+
+print (type(user))
+print("Nombre del usuario antes de peticion")
+print(user.nombre)
+
+respuesta = user.obtenerInformacion()
+
+message = input ("introduce tu mensaje: ")
+user.publicarComentario(message)
---
+++ usuario.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+class Usuario:
+	nombre = ""
+
+	def __init__(self):
+		self.url  = "https://graph.facebook.com/v2.8/me"
+		self.token = "EAACEdEose0cBAOl96KrAAXI1Cm0yhEZAp3dyRN5WjJcyfmF8xe5qRq1q3K7s3mybTNqZAfwgFnTc5JfF61oFRe2Ii9g6eUZBVcf8dibeLaKZBN98Sy9IgonOLuZBtRWGw21X8mZC45ZARbTNXurm92iQ2NCP3pZCZCgNuEj4MAQkzdAZDZD"
+
+	def obtenerInformacion(self):
+		paramentros = {"access_token": self.token}
+		resultado = requests.get(self.url, params = paramentros).json()
+		print(resultado)
+		self.nombre = resultado ["name"]
+		return resultado
+
+
+	def publicarComentario(self, message):
+		self.url = "https://graph.facebook.com/v2.8/feed"
+		paramentros = {"message": message, "access_token": self.token}
+		resultado = requests.post(self.url, params = paramentros).json()
+		print(resultado)
+		return resultado
+
+	def borrarComentario(self,
